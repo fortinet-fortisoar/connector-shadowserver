a0905e1efeeda4d8d57f061b9cf2e87004edee51
cs-shivaji-kolape <skolape@fortinet.com>
2023-12-27 16:53:36 +0530

    added some minor changes and added debug log lines

--- shadowserver/operations.py
+++ shadowserver/operations.py
@@ -14,12 +14,12 @@ class ShadowServer(object):
     def __init__(self, config):
         self.server_url = config.get('server_url').strip()
         self.headers = {'accept': 'application/json'}
-        if not self.server_url.startswith('https://'):
-            self.server_url = 'https://{0}/'.format(self.server_url)
+        if not self.server_url.startswith('https://') and not self.server_url.startswith('http://'):
+            self.server_url = 'https://' + self.server_url
 
     def make_api_call(self, endpoint=None, method='GET', headers=None, health_check=False):
         url = self.server_url + endpoint
-        logger.debug('Final url to make rest call is: {0}'.format(url))
+        logger.debug('API Service Endpoint: {0}'.format(url))
         if headers:
             self.headers.update(headers)
         try:
